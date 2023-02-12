02fbb1e8d5f84cad795dcfb6b2894d3023f05d92
tacowasabii <godseoril812@gmail.com>
2023-02-12 19:39:17 +0900

    Chore: tailwindCSS 설치

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import { GlobalStyle } from '@/styles/globals';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
