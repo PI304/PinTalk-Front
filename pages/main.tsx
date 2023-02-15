248ccdd75e80dd6daced1c68d9246ac630bba087
tacowasabii <godseoril812@gmail.com>
2023-02-15 17:52:57 +0900

    test

--- pages/main.tsx
+++ pages/main.tsx
@@ -1,3 +1,5 @@
-export default function main() {
-  return <h1 className='text-3xl font-bold underline'>test</h1>;
-}
+import React from 'react';
+
+export const main = () => {
+  return <div>main</div>;
+};
