e8f5b5b904f902de0ac44758c19943763dfde222
tacowasabii <godseoril812@gmail.com>
2023-02-22 20:53:57 +0900

    Feat: 비밀번호 찾기 페이지 구성

---
+++ pages/findpw.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+import { LoginLayout } from '@/components/common/layouts';
+import { FindPWPage } from '../components/findPW/FindPWPage';
+
+export default function findpw() {
+  return <FindPWPage />;
+}
+
+findpw.getLayout = (page: ReactElement) => {
+  return <LoginLayout>{page}</LoginLayout>;
+};
