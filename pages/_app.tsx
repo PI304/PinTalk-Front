d68380f8bb2c59d303c389f5b888cc1eab9492b7
jooooonis <xowns3213@naver.com>
2023-03-08 17:04:10 +0900

    Feat : reducer 공급

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,17 @@ import '@/styles/globals.css';
 import { ReactElement } from 'react';
 import { GlobalStyle } from '@/styles/globals';
 import { AppPropsWithLayout } from '@/@types/base';
+import { Provider } from 'react-redux';
+import store from '@/features/store';
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page: ReactElement) => page);
   return (
-    <div className='font-PretendardRegular'>
-      {/* <GlobalStyle /> */}
-      {getLayout(<Component {...pageProps} />)}
-    </div>
+    <Provider store={store}>
+      <div className='font-PretendardRegular'>
+        {/* <GlobalStyle /> */}
+        {getLayout(<Component {...pageProps} />)}
+      </div>
+    </Provider>
   );
 }
