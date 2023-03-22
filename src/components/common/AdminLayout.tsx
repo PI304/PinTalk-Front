38148ca87e80bd770c45953750e275bc826a9a7c
tacowasabii <godseoril812@gmail.com>
2023-03-23 00:47:16 +0900

    Feat: 관리자 페이지

--- src/components/common/AdminLayout.tsx
+++ src/components/common/AdminLayout.tsx
@@ -1,56 +1,71 @@
 import Link from 'next/link';
-import { svgDots, svgMessage, svgOut, svgPinTalkEmo, svgSetting } from '@styles/svg';
+import { useRouter } from 'next/router';
+import { svgMessageOff, svgOut, svgPersonOff, svgPinTalkEmo2, svgSetting } from '@styles/svg';
 import { ChildrenType } from 'types/base';
-import { svgPerson } from '@styles/svg';
+import { useAppDispatch } from '../../features/hooks';
+import { useAppSelector } from '@features/hooks';
+import { svgMessageOn, svgPersonOn } from '../../styles/svg';
+import { useEffect } from 'react';
+import { setEmail } from '@features/user/userSlice';
 
 const AdminLayout = ({ children }: ChildrenType) => {
+  const selector = useAppSelector;
+  const email = selector((state) => state.user.email);
+  const dispatch = useAppDispatch();
+
+  const router = useRouter();
+  const isChatActive = router.pathname.includes('adminChat');
+  const isProfileActive = router.pathname.includes('adminProfile');
+
+  useEffect(() => {
+    const storedData = sessionStorage.getItem('user');
+    if (storedData) {
+      const parsedData = JSON.parse(storedData);
+      dispatch(setEmail(parsedData));
+    }
+  }, [dispatch]);
+
   return (
     <div className='min-h-screen'>
       <div className='flex min-h-screen'>
-        <div className='bg-blue-main w-[220px]'>
-          <div className='font-Montserrat font-semibold  text-white text-21 mt-8 ml-9 flex'>
-            PinTalk<div className=''>{svgPinTalkEmo}</div>
-          </div>
-          <div className='text-white mt-30 flex flex-col mt-10 text-17 w-full'>
-            <div className='flex items-center py-3 bg-blue-deep pl-8'>
-              <div className='mr-3'>{svgMessage}</div>
-              <div>대화창</div>
+        <div className='bg-blue-main w-[180px] flex flex-col justify-between '>
+          <div>
+            <div className='font-Montserrat font-semibold  text-white text-21 mt-8 ml-7 flex '>
+              PinTalk<div className=''>{svgPinTalkEmo2}</div>
             </div>
-            <div className='flex items-center py-3 pl-8'>
-              <div className='mr-3'>{svgPerson}</div>내 정보
-            </div>
-            <div className='flex items-center py-3 pl-8'>
-              <div className='mr-3'>{svgSetting}</div>
-              <div>환경설정</div>
-            </div>
-            <Link
-              href='/login'
-              className='flex items-center justify-center w-[100px] mx-auto mt-[640px]'>
-              <div className='pr-3'>{svgOut}</div>
-              <div>로그아웃</div>
-            </Link>
-          </div>
-        </div>
-        <div className='bg-BG-2 flex'>
-          <div className='w-[520px] pt-10 px-10'>
-            <div className='flex justify-between items-center text-18 text-text-3'>
-              <div>최근 대화내역</div>
-              <div>{svgDots}</div>
-            </div>
-            <div className='w-[440px] h-[100px] bg-white mt-4 rounded-xl flex items-center justify-between px-6'>
-              <div className='flex items-center'>
-                <img className='my-auto' src='/userImg.svg' alt='userImg' width={40} height={30} />
-                <div className='ml-5'>
-                  <div className='font-PretendardSemibold text-text-2'>유저1</div>
-                  <div className='text-text-4 text-14'>메시지</div>
+            <div className='text-white mt-30 flex flex-col mt-10 text-16 w-full'>
+              <div>
+                <Link href={`/adminChat/${encodeURIComponent(email)}`}>
+                  <div
+                    className={`flex items-center py-3 pl-7 ${isChatActive ? 'bg-blue-deep' : ''}`}>
+                    <div className='mr-3'>{isChatActive ? svgMessageOn : svgMessageOff}</div>
+                    <div>대화창</div>
+                  </div>
+                </Link>
+                <Link href={`/adminProfile/${encodeURIComponent(email)}`}>
+                  <div
+                    className={`flex items-center py-3 pl-7 ${
+                      isProfileActive ? 'bg-blue-deep' : ''
+                    }`}>
+                    <div className='mr-3'>{isProfileActive ? svgPersonOn : svgPersonOff}</div>내
+                    정보
+                  </div>
+                </Link>
+                <div className='flex items-center py-3 pl-7'>
+                  <div className='mr-3'>{svgSetting}</div>
+                  <div>환경설정</div>
                 </div>
               </div>
-              <div className='text-text-6 text-12'>30분전</div>
             </div>
           </div>
-          <div className='flex-1 flex justify-end'>
-            <div className='bg-white w-full'>sdskjfskdfkds</div>
-          </div>
+          <Link
+            href='/login'
+            className='flex items-center justify-center w-[100px] mx-auto text-white text-16 mb-10'>
+            <div className='pr-3'>{svgOut}</div>
+            <div>로그아웃</div>
+          </Link>
+        </div>
+        <div className='bg-BG-2 w-[calc(100%-180px)]'>
           <div>{children}</div>
         </div>
       </div>
