fe3b88bc45e5529b99e6fff2011149278e5c36f7
tacowasabii <godseoril812@gmail.com>
2023-04-12 02:17:58 +0900

    Style: 스타일 수정

--- src/components/description/CodeContent.tsx
+++ src/components/description/CodeContent.tsx
@@ -13,7 +13,7 @@ const CodeContent = ({ num, title, content, code }: codeContent) => {
       <div className='md:w-[24px] md:h-[24px] w-[30px] h-[30px] bg-blue-main rounded-full flex justify-center items-center mr-3'>
         <div className='font-PretendardSemibold md:text-12 text-16 text-white'>{num}</div>
       </div>
-      <div className='md:w-[385px] xl:w-[730px] w-[1100px]'>
+      <div className='md:w-[320px] xl:w-[730px] w-[1100px]'>
         <div className='font-PretendardMedium md:text-18 text-20 text-text-2'>{title}</div>
         <div className='md:text-15 xl:text-17 text-20 text-text-3 md:my-1 my-3 xl:my-2'>
           {content}
