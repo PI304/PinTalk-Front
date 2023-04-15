c50c92b443e99d6069c67cb519029ea80b72d317
tacowasabii <godseoril812@gmail.com>
2023-04-15 19:48:07 +0900

    Feat:라우터 주소 변경

--- src/pages/adminProfile/[id].tsx
+++ src/pages/adminProfile/[id].tsx
@@ -1,17 +1,14 @@
 import { ReactElement } from 'react';
-import { AdminChatPage } from '@components/adminChat';
 import { AdminLayout } from '@components/common';
-import { useRouter } from 'next/router';
+import { AdminProfilePage } from '@components/adminProfile';
 import CustomHead from '@seo/CustomHead';
 import Seo from '@constants/seo';
 
 export default function main() {
-  const router = useRouter();
-  const { email } = router.query;
   return (
     <>
-      <CustomHead title={Seo.Title.chat} />
-      <AdminChatPage adminEmail={email} />
+      <CustomHead title={Seo.Title.profile} />
+      <AdminProfilePage />
     </>
   );
 }
