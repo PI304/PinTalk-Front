366fea4dbd4c2baeded0a44b523331c50a4308b7
tacowasabii <godseoril812@gmail.com>
2023-04-20 01:54:51 +0900

    Feat: api 추가

--- src/apis/user/userApi.ts
+++ src/apis/user/userApi.ts
@@ -1,11 +1,34 @@
 import instance from '@apis/_axios/instance';
-import { UserId } from './userApi.type';
+import fileinstance from '@apis/_axios/instance';
+import { UserId, UserImage, UserProfile } from './userApi.type';
 
 export class UserApi {
   async getUserDataById({ id }: UserId) {
     const { data } = await instance({
       method: 'GET',
       url: `users/${id}/`,
+    });
+    return data;
+  }
+  async patchUserDataById({ id }: UserId, userProfile: UserProfile) {
+    const { data } = await instance({
+      method: 'PATCH',
+      url: `users/${id}/`,
+      data: userProfile,
+    });
+    return data;
+  }
+  async patchUserImageById({ id }: UserId, userImage: UserImage) {
+    const formData = new FormData();
+    formData.append('profile_image', userImage.profileImage);
+
+    const { data } = await instance({
+      method: 'PATCH',
+      url: `users/${id}/`,
+      data: formData,
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
     });
     return data;
   }
